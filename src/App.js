1df1efd8fd5a9aa0e256dda371347e8bd9e05960
Ajay Yadav <musicalworld313@gmail.com>
2022-08-04 19:12:02 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
+import Posts from './Posts';
+import Pagination from './Pagination';
 
 function App() {
+  const [currentPage,setCurrentPage]=useState(0);
+  const [lastCountPage,setLastCountPage]=useState(0);
+  const [dataPerPage, setDataPerPage] = useState(10);
+  const [posts,setPosts]=useState([]);
+  const [loading,setLoading]=useState(false);
+  useEffect(()=>{
+    const fetchData=async()=>{
+      setLoading(true);
+      const res=await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+    fetchData();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My App</h1>
+      <Posts post={posts} loading={loading} lastCountPage={lastCountPage} dataPerPage={dataPerPage} setDataPerPage={setDataPerPage} setLastCountPage={setLastCountPage} currentPage={currentPage}/>
+      <Pagination post={posts} loading={loading} lastCountPage={lastCountPage} dataPerPage={dataPerPage} setDataPerPage={setDataPerPage} setLastCountPage={setLastCountPage} setCurrentPage={setCurrentPage}/>
     </div>
   );
 }
